654715a3fa30a505bac227ce3c8ba9a6875813d0
sajalkhan <sajalhossen@yahoo.com>
2024-09-23 21:42:03 +0600

    init setup and setup custom fonts

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to donation app</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
